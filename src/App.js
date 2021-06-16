fa458c97545c0256f9c1c3499fa37879100149fd
aleupons <apahoquei@gmail.com>
2021-06-16 19:55:20 +0200

    Array d'items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { Formulari } from "./components/Formulari";
 import { Llista } from "./components/Llista";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, nom: "Còctel molotov", utilitat: "Cremar contenidors" },
+    { id: 2, nom: "Passamuntanyes", utilitat: "Evitar identificacions" },
+    { id: 3, nom: "Casc", utilitat: "No perdre ulls" },
+    {
+      id: 4,
+      nom: "Xibeca",
+      utilitat: "Debilitar policies i estómacs",
+    },
+  ]);
   return (
     <>
+      <h1>Kit d'autodefensa</h1>
       <Formulari />
       <Llista />
     </>
